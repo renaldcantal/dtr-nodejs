ba4998f5d597f7d4fd16d156d6411f07ce38635c
Renald Leif Cantal <renaldcantal>
2021-05-05 10:27:47 +0800

    make it better

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.set('view engine', 'hbs');
 
 app.listen(port, () => console.log(`App running on ${port}`))
 
-app.get('/inventory', systemController);
-app.get('/list', systemController);
+app.use('/inventory', systemController);
+app.use('/list', systemController);
