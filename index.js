7cccb19fd9851393ad291ce7eb11265d1a4f31ed
Renald Leif Cantal <renaldcantal>
2021-05-05 10:20:42 +0800

    Update

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.set('view engine', 'hbs');
 
 app.listen(port, () => console.log(`App running on ${port}`))
 
-app.use('/', systemController);
+app.use('/inventory', systemController);
 app.use('/list', systemController);
